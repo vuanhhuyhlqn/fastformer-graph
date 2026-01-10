d838ea7c1a0d880e19828964935c7a7563e48751
madmoon <madmoontokyo@gmail.com>
2026-01-10 13:08:17 +0700

    gat + gcn

--- gnn/__init__.py
+++ gnn/__init__.py
@@ -1,1 +1,3 @@
 from .sage import create_sage
+from .gat import create_gat
+from .gcn import create_gcn
---
+++ gnn/gat.py
@@ -0,0 +1,57 @@
+"""
+Code Reference: https://github.com/victorchen96/ReNode/blob/main/transductive/network/gat.py
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GATConv
+
+class StandGATX(nn.Module):
+    def __init__(self, nfeat, nhid, dropout, nlayer=3):
+        super(StandGATX, self).__init__()
+
+        num_head = 4
+        head_dim = nhid // num_head
+
+        self.conv1 = GATConv(nfeat, head_dim, heads=num_head)
+        self.conv2 = GATConv(nhid, nhid)
+        self.convx = nn.ModuleList(
+            [GATConv(nhid, head_dim, heads=num_head) for _ in range(nlayer - 2)]
+        )
+        self.dropout_p = dropout
+
+        self.reg_params = list(self.conv1.parameters()) + list(self.convx.parameters())
+        self.non_reg_params = self.conv2.parameters()
+
+    def get_embed(self, x, adj):
+        edge_index = adj
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        for iter_layer in self.convx:
+            x = F.dropout(x, p=self.dropout_p, training=self.training)
+            x = iter_layer(x, edge_index)
+            x = F.relu(x)
+        return x
+
+    def embed_to_pred(self, x, adj):
+        edge_index = adj
+        x = self.conv2(x, edge_index)
+        return x
+
+    def forward(self, x, adj):
+        edge_index = adj
+
+        x = F.relu(self.conv1(x, edge_index))
+
+        for iter_layer in self.convx:
+            x = F.dropout(x, p=self.dropout_p, training=self.training)
+            x = F.relu(iter_layer(x, edge_index))
+
+        x = F.dropout(x, p=self.dropout_p, training=self.training)
+        x = self.conv2(x, edge_index)
+
+        return x
+
+def create_gat(nfeat, nhid, dropout, nlayer):
+    model = StandGATX(nfeat, nhid, dropout, nlayer)
+    return model
